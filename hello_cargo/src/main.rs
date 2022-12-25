0d3950134f82fd41f3c3c96f370032558c029656
Stone C. Lasley <stonelasley@gmail.com>
2022-12-25 13:41:32 -0700

    feat(1.3): Hello Cargo

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{}", hello_world());
+}
+
+fn hello_cargo() -> &'static str {
+    return "Hello, Cargo!";
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_hello_cargo() {
+        assert_eq!(hello_cargo(), "Hello, Cargo!");
+    }
+}
