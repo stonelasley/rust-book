2737e0da0c5af2d6b9b012fe82e64a3b3da6e1a1
Stone C. Lasley <stonelasley@gmail.com>
2022-12-26 17:55:15 -0700

    feat(2): Guessing Game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the Number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
+
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Large!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+
+        println!("You guessed {guess}")
+    }
+}
