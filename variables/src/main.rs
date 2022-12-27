2b49ff41a4df556bcf1444467400716dc2ea2451
Stone C. Lasley <stonelasley@gmail.com>
2022-12-27 09:59:51 -0700

    feat(3.1): variables

---
+++ variables/src/main.rs
@@ -0,0 +1,17 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+
+    let y = 10;
+    {
+        let mut y = y + 5;
+        y = y + 1;
+        println!("The value of y inside scope is {y}");
+    }
+    println!("The value of y outside scope is {y}");
+
+}
